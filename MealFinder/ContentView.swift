b1eaabddbe6a9ba0f81e1869b765f3f3af2ad565
Jacob Conacher <jake.conacher@gmail.com>
2024-06-25 18:40:16 -0500

    Implemented simple dynamic list

--- MealFinder/ContentView.swift
+++ MealFinder/ContentView.swift
@@ -7,13 +7,24 @@
 
 import SwiftUI
 
+struct Dessert: Identifiable {
+    let name: String
+    let id = UUID()
+}
+
+private var desserts = [
+    Dessert(name: "Chocolate Cake"),
+    Dessert(name: "Brownie"),
+    Dessert(name: "Cheescake"),
+    Dessert(name: "Ice Cream")
+]
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(desserts){
+                Text($0.name)
+            }
         }
         .padding()
     }
