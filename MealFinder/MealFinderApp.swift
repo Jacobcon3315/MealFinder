09247444ba39b54cf5da4920b5ae9681113c6d01
Jacob Conacher <jake.conacher@gmail.com>
2024-06-25 13:09:32 -0500

    Initial Commit

---
+++ MealFinder/MealFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealFinderApp.swift
+//  MealFinder
+//
+//  Created by Jabie on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MealFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
